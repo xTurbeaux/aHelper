8471d6c8b0a476c3e9d1f70c40db3222fbb57cf7
Aaron Mayhew <standingbytocopy@gmail.com>
2021-05-04 15:00:43 -0500

    beginning animatin of slogan to fade in, and then bloom to app sign-in

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Animated } from 'react-native';
 
 export default function App() {
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000
+    }).start();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.view}>
         <View style={styles.back}>
           <View style={styles.bc}>
-            <Text style={styles.text}>In Space, no one can hear you scream</Text>
+
+            <Animated.View
+              style={[
+                styles.fadingContainer,
+                {
+                  // Bind opacity to animated value
+                  opacity: this.state.fadeAnim
+                }
+              ]}
+            >
+                <Text style={styles.text}>In Space, no one can hear you scream</Text>
+            </Animated.View>
+
           </View>
         </View>
       </View>
@@ -33,6 +63,10 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     color: '#41b3eb'
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue"
   },
   text: {
     fontFamily: "Helvetica Neue",
